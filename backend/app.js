198e553f4855ec362271749dba16baff5c4c2b41
jenmwa <jenmwa@gmail.com>
2023-03-20 12:38:55 +0100

    setup project front & back

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const mongoose = require('mongoose');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const productsRouter = require('./routes/products');
+const ordersRouter = require('./routes/orders');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/jenny-waller', {
+   useUnifiedTopology: true
+})
+.then (() => {
+  console.log('We´re connected to the database!');
+
+})
+.catch(err => console.log('err',err));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('api/users', usersRouter);
+app.use('api/products', productsRouter);
+app.use('api/orders', ordersRouter);
+
+module.exports = app;
